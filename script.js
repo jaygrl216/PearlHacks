82470c1529524383a105cdaba5f0111d81fbd254
Jada <jwashin7@terpmail.umd.edu>
2015-03-22 11:52:56 -0400

    added script

---
+++ script.js
@@ -0,0 +1,35 @@
+var total = 0;
+var total1=0;
+var total2=0;
+var total3=0;
+var budget=0;
+var remain = 0;
+
+
+function budgetAdd(){
+	budget = budget + parseInt(document.saver.budget.value);
+	document.getElementById("budget").innerHTML = "Your budget is: $" + budget;
+}
+
+function addTotals() {
+	var x = parseInt(document.saver1.price.value);
+	if(document.saver1.category.value == "food"){
+	total1 = total1 + x;
+	}
+	else if(document.saver1.category.value=="clothes"){
+		total2 = total2 + x;
+	} else {
+		total3 = total3 + x;
+	}
+
+}
+
+function submit() {
+	total= total1 + total2 + total3;
+	remain = budget - total;
+    document.getElementById("food").innerHTML = "Spent on food: $" + total1;
+    document.getElementById("clothes").innerHTML = "Spent on clothes: $" + total2;
+    document.getElementById("makeup").innerHTML ="Spent on Makeup: $" + total3;
+    document.getElementById("all").innerHTML ="Your collective total is: $" + total;
+    document.getElementById("r").innerHTML = "Remaining Budget: $" + remain;
+}
