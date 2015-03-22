880c84d1021276a025e6b7beadf5d024875f1b19
Jada <jada216@gmail.com>
2015-03-21 23:44:59 -0400

    Changed font

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+var totalspent = 0;
+var categories = ["Food"];
+var price = 0;
 
 function submit() {
-    document.getElementById("data").innerHTML = "This is what you spent: ";
+    document.getElementById("data").innerHTML = "This is what you spent: $" + price;
+
+}
+
+function printTotals() {
+
 }
