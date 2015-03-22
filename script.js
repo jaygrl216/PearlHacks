73f4c6806ebff7ff2f29679f9fdc0f0438d84547
Jada <jada216@gmail.com>
2015-03-22 01:46:35 -0400

    edited javascript

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 var ans;
 var amount = 0;
 var menuChoice;
-var total = 0
+var total = 0;
 var total1=0;
 var total2=0;
 var total3=0;
-var total4=0
+var total4=0;
 var price = 0;
 
-function getprice() {
-	total = total + form.price.value;
+function addTotals() {
+	total = total + document.getElementById("cost").value;
+
 }
+
 function submit() {
     document.getElementById("data").innerHTML = "This is what you spent: $" + total;
 }
