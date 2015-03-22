71e6915d63a05ace99eed95aad4f1b944c9ed09f
Jada <jada216@gmail.com>
2015-03-22 01:24:02 -0400

    Revert "I forgot"
    This reverts commit 83ddf96e8466c8bbb313a11d19161401340f3a92.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var price = 0;
 
 function submit() {
-    document.getElementById("data").innerHTML = "This is what you spent: $" + price;
+    document.getElementById("data").innerHTML = "This is what you spent: ";
 }
