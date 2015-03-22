70696370e0cd4d771b581ba636e4a8412e6fe108
Jada <jada216@gmail.com>
2015-03-22 00:00:32 -0400

    Revert "Changed font"
    This reverts commit 880c84d1021276a025e6b7beadf5d024875f1b19.

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-var totalspent = 0;
-var categories = ["Food"];
-var price = 0;
 
 function submit() {
-    document.getElementById("data").innerHTML = "This is what you spent: $" + price;
-
-}
-
-function printTotals() {
-
+    document.getElementById("data").innerHTML = "This is what you spent: ";
 }
