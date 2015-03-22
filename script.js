14166f99eaefdc7bc43813f8e20b1f320bf2b8a0
lmurra7 <lisanicolemurray@yahoo.com>
2015-03-22 01:22:02 -0400

    added variables

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-
+var ans;
+var amount = 0;
+var menuhChoice;
+var total = 0
+var total1=0;
+var total2=0;
+var total3=0;
+var total4=0
 function submit() {
     document.getElementById("data").innerHTML = "This is what you spent: ";
 }
